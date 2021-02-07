afa7e37a58fed4e156e15b8ba4981ef19c261b60
AlexBehl <alexanderbehl@icloud.com>
2021-02-07 18:25:08 +0100

    fixing auth

--- webserver/gateway/src/main/java/edu/tum/ase/gateway/configuration/OAuth2RestTemplateConfiguration.java
+++ webserver/gateway/src/main/java/edu/tum/ase/gateway/configuration/OAuth2RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package edu.tum.ase.gateway.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OAuth2RestTemplateConfiguration {
   @Bean
-  public OAuth2RestOperations restTemplate(OAuth2ClientContext context) {
+  public OAuth2RestOperations restTemplate(@Qualifier("oauth2ClientContext") OAuth2ClientContext context) {
     ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
     return new OAuth2RestTemplate(details, context);
   }
--- webserver/gateway/src/main/java/edu/tum/ase/gateway/configuration/WebSecurityConfiguration.java
+++ webserver/gateway/src/main/java/edu/tum/ase/gateway/configuration/WebSecurityConfiguration.java
@@ -16,23 +16,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .csrf()
-      .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
       .and()
-      .authorizeRequests()
-      .antMatchers("/authenticated").permitAll()
-      .antMatchers("/compiler/**").authenticated()
-      .antMatchers("/project/**").authenticated()
-      .antMatchers("/dark-mode").authenticated()
+        .authorizeRequests()
+        .antMatchers("/authenticated").permitAll()
+        .antMatchers("/compiler/**").authenticated()
+        .antMatchers("/project/**").authenticated()
+        .antMatchers("/dark-mode").authenticated()
       .and()
-      .logout()
-      .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-      .logoutUrl("/logout")
-      .invalidateHttpSession(true)
-      .clearAuthentication(true)
-      .deleteCookies("JSESSIONID")
-      .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutUrl("/logout")
+        .invalidateHttpSession(true)
+        .clearAuthentication(true)
+        .deleteCookies("JSESSIONID")
+        .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+          httpServletResponse.setStatus(HttpServletResponse.SC_OK);
       });
   }
 }
